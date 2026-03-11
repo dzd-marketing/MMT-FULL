19fe7dc5f43d947603cc33b9b3f5086de8409f52
mmtbusinesshub <mmtbusinesshub@gmail.com>
2026-03-11 15:07:57 +0530

    hi

--- FRONTEND/test-api.js
+++
@@ -1,5 +0,0 @@
-fetch('http://localhost:3000/api/translate', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ texts: ["Highest Quality Services", "Real results, no bots"], to: "si" })
-}).then(res => res.json()).then(console.log).catch(console.error);
--- FRONTEND/test-translate.js
+++
@@ -1,7 +0,0 @@
-import translate from 'google-translate-api-x';
-
-translate(['Ik spreek Engels', 'Hoe gaat het?'], {to: 'en'}).then(res => {
-    console.log(res.map(r => r.text));
-}).catch(err => {
-    console.error("Error:", err);
-});
