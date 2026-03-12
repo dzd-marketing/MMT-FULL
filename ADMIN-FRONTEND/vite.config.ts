56861feb73f60ad1f92706cece97385f765a2599
dzd-marketing <sitewasd2026@gmail.com>
2026-03-12 12:32:40 +0530

    Update vite.config.ts

--- ADMIN-FRONTEND/vite.config.ts
+++ ADMIN-FRONTEND/vite.config.ts
@@ -17,10 +17,9 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: 3000,
-      // ✅ PROXY SETUP එක add කරන්න
       proxy: {
         '/api': {
-          target: 'http://localhost:5000',
+          target: 'https://mmtsmmpanel.cyberservice.online',
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path,
@@ -37,8 +36,6 @@ export default defineConfig(({ mode }) => {
           },
         }
       },
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
